f110b3d6ee2fa2f2f8e707962542d5aabe01a877
Brent Shay Lopez <brentshay.lopez@gmail.com>
2017-01-29 22:06:46 -0500

    Add skills section to resume page

--- app/resume/controllers/ResumeCtrl.js
+++ app/resume/controllers/ResumeCtrl.js
@@ -1,3 +1,109 @@
-angular.module('app.resume').controller('ResumeCtrl', function(){
+"use strict";
+
+angular.module('app.resume').controller('ResumeCtrl', function($scope){
+	$scope.progLanguages = [
+		{
+			level: "Fluent",
+			langs: [
+				'C#',
+				'HTML',
+				'JavaScript',
+				'CSS',
+				'C++'
+			]
+		},
+		{
+			level: "Proficient",
+			langs: [
+				'Java',
+				'C',
+				'PHP'
+			]
+		}
+	];
 	
+	
+	$scope.engineImagePaths = [
+		"images/unity-logo-rgb.png",
+		"images/Unreal_Engine_Horiz_Black.png"
+	];
+	
+	$scope.creativeSoftware = [
+		{
+			brand: "Adobe",
+			programs: [
+				{
+					name: "Photoshop CS6",
+					tags: [
+						"Photo-Editing",
+						"Texture Editing"
+					]
+				},
+				{
+					name: "Premiere Pro CS6",
+					tags: [
+						"Video Editing"				
+					]
+				},
+				{
+					name: "After Effects CS6",
+					tags: [
+						"Video Composition",
+						"Motion Graphics",
+						"VFX"
+					]
+				}
+			]
+			
+		},
+		{
+			brand: "Avid",
+			programs: [
+				{
+					name: "Pro Tools",
+					tags: [
+						"Audio Mixing",
+						"Audio Editing"
+					]
+				}
+			]
+			
+		},
+		{
+			brand: "Autodesk",
+			programs: [
+				{
+					name: "Maya",
+					tags: [
+						"Low-Poly Modeling",
+						"High-Poly Modeling",
+						"3D Animation"
+					]
+				}
+			]
+			
+		}
+	];
+	
+	$scope.spokenLangs = [
+		{
+			level: "Fluent",
+			langs: [
+				"English",
+				"Spanish"
+			]
+		},
+		{
+			level: "Proficient",
+			langs: [
+				
+			]
+		},
+		{
+			level: "Conversational",
+			langs: [
+				"French"
+			]
+		}
+	];
 });
